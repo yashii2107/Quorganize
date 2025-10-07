6ef7997d2e3f004bbc10b762c98e84327e85f98e
yashii2107 <yashii2107@gmail.com>
2025-10-07 17:13:44 +0530

    model added

---
+++ src/models/question.model.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const QuestionSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    solution: {type:String},
+    section:  {type: String, enum: ["todo","completed"], default: "todo"},
+    difficulty: {type: String, enum: ["Easy", "Medium", "Hard"], default: "Easy"},
+    tags: { type: [String], default: [] }      // Optional tags (e.g., array, dp)
+}, { timestamps: true });
+export default mongoose.model("Question", QuestionSchema);
